b4ebe544b532520adcf893200d478e7c1167777a
Memoria369 <jhoan.mauricio@gmail.com>
2021-09-26 14:48:06 -0500

    Ejercicios Extras

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,13 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+//var res = Object.entries(objeto);
+//return res;
+var array = [];
+for (clave in objecto){
+  array = push([clave, objecto[clave]]);
+}
+  return array;
 }
 
 
@@ -18,7 +25,31 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+var miCadena = string;
+var caracter = miCadena.length;
+let cadena = [];
+let final = [];
+let resultado = [];
+var letra = 0;
+var cuenta = 0;
+
+for (var i = 0; i < caracter; i++){
+  letra = miCadena.charAt(i);
+  unica.push(letra);
+
+  posicion = miCadena.indexOf(letra);
+    while ( posicion != -1 ) {
+        cuenta++;
+        posicion = miCadena.indexOf(letra,posicion+1);
+    }
+
+  cadena = (letra + ':' + cuenta);
+  final.push(cadena);
+  cuenta = 0;
+  }
+  console.log(final);
 }
+
 
 
 function capToFront(s) {
@@ -26,7 +57,27 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+let masyuscula = [];
+let minuscula = [];
+let min = [];
+let final = [];
+for(var index = 0; index < s.length; index++)
+{
+  var letraActual = s.charAt(index);
+    if (letraActual === letraActual.toUpperCase()){ 
+      mayuscula = letraActual;
+      final.push(mayuscula);}
+
+    if (letraActual === letraActual.toLowerCase()){
+      minuscula = letraActual;
+      min.push(minuscula);}          
 }
+
+final = final.concat(min);
+return final.join("");
+}
+
+
 
 
 function asAmirror(str) {
@@ -35,6 +86,27 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+var palabras = [];
+var canPalabras = 0;
+var canLetras = 0; 
+var letras = 0;
+var nueva = [];
+
+
+palabras = str.split(' ');
+canPalabras = palabras.length;
+palabras.reverse();
+
+for (var p = 0; p < canPalabras; p++){
+  canLetras = palabras[p].length;
+  for (var i = 0; i < canLetras; i++){
+    letras = palabras[p];
+    nueva.unshift(letras[i]);
+    }
+  nueva.unshift('  ');
+  }
+  nueva.shift();
+  return (nueva.join (' '));
 } 
 
 
@@ -43,6 +115,12 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  numero = '' + numero + '';
+  largo = numero.length;
+
+    if ( numero[0] === numero[largo - 1]){
+      return 'Es capicua';}
+    else{return 'No es capicua';}
 }
 
 
@@ -50,6 +128,7 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
 }
 
 
@@ -57,6 +136,13 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+let words = arr;
+
+words.sort((a,b) =>{
+   return a.length - b.length;
+});
+return words;
 }
 
 
@@ -66,6 +152,21 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+var nuevo = [];
+nuevo = arreglo1.concat(arreglo2);
+
+var obj = {};
+var dup = [];
+
+ for(var i = 0; i < nuevo.length; i++){
+  if(!obj[nuevo[i]]){
+    obj[nuevo[i]] = 1;
+  }else{
+    dup.push(nuevo[i]);
+  }
+   }
+return dup;
 }
 
 
